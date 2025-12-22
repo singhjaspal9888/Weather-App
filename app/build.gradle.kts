21eac4d4ffac4652d348d3266071381e6dcc362b
Jaspal Singh <jaspalsinghbhakha@gmail.com>
2025-12-22 09:57:55 +0530

    Update applicationId for internal app sharing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.weatherapp"
+        applicationId = "com.weatherapp"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
